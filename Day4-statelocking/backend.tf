fac9fceb5b46b5ae847dc226c8e38e20710d3e43
hari <hari4cs@gmail.com>
2026-03-26 23:03:10 +0530

    modified backedn.tf

--- Day4-statelocking/backend.tf
+++ Day4-statelocking/backend.tf
@@ -3,5 +3,5 @@ terraform {
     bucket = "remotebackendtestng"
     key    = "Day-4/terraform.tfstate"
     region = "ap-south-1"
-  }
+    use_lockfile = true   #this features suports only  terraform version 1.10 and above only
 }
